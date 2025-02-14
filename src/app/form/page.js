e187a599013ea4ce69a9563bc47c54c4da0d94da
Josh Prunty <joshprunty@gmail.com>
2025-02-14 11:23:48 -0600

    more css

--- src/app/form/page.js
+++ src/app/form/page.js
@@ -110,6 +110,6 @@ export default function FormWrapper() {
     }
   };
 
-  return <div className="h-screen max-w-7xl mx-auto px-4 flex flex-col justify-center items-center">{renderStep()}</div>;
+  return <div className="h-screen max-w-[100rem] border border-red-500 mx-auto px-4 flex flex-col justify-center items-center">{renderStep()}</div>;
 }
 
--- src/formSteps/Form1A.jsx
+++ src/formSteps/Form1A.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { Button } from "@/components/ui/button";
 
@@ -9,17 +8,15 @@ function Form1A({ nextStep }) {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center w-1/2">
-      <div className="w-7/8 d-grid gap-2">
-        <h1>Find your next course in under 5 minutes.</h1>
-        <p>
-          Tell us about what you're looking for in upcoming classes, which
-          requirements you'd like to fulfill, and previous courses you've taken.
-        </p>
-        <Button variant="secondary" size="lg" onClick={handleStartClick}>
-          Start Assessment
-        </Button>
-      </div>
+    <div className="flex flex-col justify-center">
+      <p className="text-5xl font-medium">Find your next course in under 5 minutes.</p>
+      <p className="text-xl font-light">
+        Tell us about what you're looking for in upcoming classes, which
+        requirements you'd like to fulfill, and previous courses you've taken.
+      </p>
+      <Button variant="secondary" size="lg" className="w-full" onClick={handleStartClick}>
+        Start Assessment
+      </Button>
     </div>
   );
 }
